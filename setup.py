3ab2d12602ffc21ed6757fc65d2f415a0d94def9
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-13 12:07:02 -0400

    Add entry points for testing.

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     install_requires=requires,
     test_requires=requires,
     test_suite='run_tests.main',
+    entry_points=entry_pt,
     classifiers=[
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
