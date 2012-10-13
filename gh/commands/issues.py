96900cf1d6785df34fe8b650ad6adba9cce4878e
Ian Cordasco <graffatcolmingov@gmail.com>
2012-10-13 12:07:42 -0400

    Add more functionality to issues and pulls.
    Allow people to comment on issues. Allow people to get details on pulls and
    list review comments.

--- gh/commands/issues.py
+++ gh/commands/issues.py
@@ -1,5 +1,8 @@
 from gh.base import Command
-from gh.util import tc, wrap
+from gh.util import tc, wrap, trim_numbers, mktmpfile, rmtmpfile, sep
+from github3 import GitHubError
+from os import system
+from os.path import expandvars
 
 
 class IssuesCommand(Command):
@@ -9,11 +12,12 @@ class IssuesCommand(Command):
     summary = 'Interact with the Issues API'
     fs = ('#{bold}{0.number}{default} {0.title:.36} - @{u.login}')
     subcommands = {
-            '[#]1': 'Print the full issue',
-            '[#]1 comments': 'Print all the comments on this issue',
-            '[#]1 close': 'Close this issue',
-            '[#]1 reopen': 'Reopen this issue',
-            '[#]1 assign <assignee>': 'Assign this issue to @<assignee>',
+            '[#]num': 'Print the full issue',
+            '[#]num comments': 'Print all the comments on this issue',
+            '[#]num close': 'Close this issue',
+            '[#]num reopen': 'Reopen this issue',
+            '[#]num assign <assignee>': 'Assign this issue to @<assignee>',
+            '[#]num comment': 'Comment on this issue using $EDITOR',
             }
 
     def __init__(self):
@@ -57,20 +61,14 @@ class IssuesCommand(Command):
         opts, args = self.parser.parse_args(args)
 
         if not args:
-            iter = self.repo.iter_issues(opts.milestone, opts.state,
-                    direction=opts.direction, mentioned=opts.mentioned,
-                    number=opts.number)
-            for i in iter:
-                print(self.format_short_issue(i))
-            return 0
+            return self.print_issues(opts)
 
-        if args[0].startswith('#'):
-            args[0] = args[1:]
+        args[0] = trim_numbers(args[0])
 
         if not args[0].isdigit():
-            return -1
+            return self.COMMAND_UNKNOWN
 
-        status = 0
+        status = self.SUCCESS
         number = args.pop(0)
 
         subcommand = None
@@ -80,6 +78,8 @@ class IssuesCommand(Command):
         if not subcommand:
             issue = self.repo.issue(number)
             print(self.format_long_issue(issue))
+        elif 'comment' == subcommand:
+            status = self.comment_on(number)
         elif 'comments' == subcommand:
             status = self.print_comments(number, opts)
         elif 'close' == subcommand:
@@ -89,12 +89,11 @@ class IssuesCommand(Command):
         elif args and ('assign' == subcommand):
             self.assign(number, args[0])
         else:
-            print('Unrecognized subcommand "{0}"'.format(subcommand))
-            self.help()
-            status = -1
+            status = self.COMMAND_UNKNOWN
 
         return status
 
+    # Formatting and printing
     def format_comment(self, comment):
         fs = '@{uline}{u.login}{default} -- {date}\n{body}\n'
         body = '\n'.join(wrap(comment.body_text))
@@ -104,7 +103,6 @@ class IssuesCommand(Command):
 
     def format_long_issue(self, issue):
         format_str = '{header}\n{sep}\n{body}\n'
-        sep = '-' * 78
         title = self.format_short_issue(issue)
         body = '\n'.join(wrap(issue.body_text))
         return format_str.format(header=title, sep=sep, body=body)
@@ -116,7 +114,7 @@ class IssuesCommand(Command):
         if issue.assignee:
             extra.append(issue.assignee.login)
         if extra:
-            extra = '(' + ' -- '.join(extra) + ')'
+            extra = ' (' + ' -- '.join(extra) + ')'
         else:
             extra = ''
         return self.fs.format(issue, u=issue.user, bold=tc['bold'],
@@ -125,36 +123,68 @@ class IssuesCommand(Command):
     def print_comments(self, number, opts):
         issue = self.repo.issue(number)
         if not issue:
-            return -1
+            return self.FAILURE
         for c in issue.iter_comments(opts.number):
             print(self.format_comment(c))
-        return 0
+        return self.SUCCESS
 
+    def print_issues(self, opts):
+        status = self.SUCCESS
+        iter = self.repo.iter_issues(opts.milestone, opts.state,
+                direction=opts.direction, mentioned=opts.mentioned,
+                number=opts.number)
+        try:
+            for i in iter:
+                print(self.format_short_issue(i))
+        except GitHubError as ghe:
+            if ghe.code == 410:
+                print(ghe.message)
+                status = self.FAILURE
+            else:
+                raise ghe
+        return status
+
+    # Administration/interaction
     def _get_authenticated_issue(self, number):
         self.login()
         user, repo = self.repository
         return self.gh.issue(user, repo, number)
 
+    def comment_on(self, number):
+        issue = self._get_authenticated_issue(number)
+        name = ''
+        status = self.SUCCESS
+        if not expandvars('$EDITOR'):
+            print("$EDITOR not set")
+            return self.FAILURE
+        with mktmpfile('gh-issuecomment-') as fd:
+            name = fd.name
+            system('$EDITOR {0}'.format(fd.name))
+        if not issue.create_comment(open(name).read()):
+            status = self.FAILURE
+        rmtmpfile(name)
+        return status
+
     def close_issue(self, number):
         issue = self._get_authenticated_issue(number)
         if not issue:
-            return -1
+            return self.FAILURE
         issue.close()
-        return 0
+        return self.SUCCESS
 
     def reopen_issue(self, number):
         issue = self._get_authenticated_issue(number)
         if not issue:
-            return -1
+            return self.FAILURE
         issue.reopen()
-        return 0
+        return self.SUCCESS
 
     def assign(self, number, assignee):
         issue = self._get_authenticated_issue(number)
         if not issue:
-            return -1
+            return self.FAILURE
         issue.assign(assignee)
-        return 0
+        return self.SUCCESS
 
 # Ensures this ends up in gh.base.commands
 IssuesCommand()
--- gh/commands/pulls.py
+++ gh/commands/pulls.py
@@ -1,5 +1,6 @@
 from gh.base import Command
-from gh.util import tc
+from gh.util import tc, trim_numbers, sep, wrap
+from github3 import GitHubError
 
 
 class PullsCommand(Command):
@@ -8,7 +9,10 @@ class PullsCommand(Command):
             ' [sub-command]')
     summary = 'Interact with the Pull Requests API'
     fs = ('#{bold}{0.number}{default} {0.title:.36} - @{u.login}')
-    subcommands = {}
+    subcommands = {
+            '[#]num': 'Print full information about specified pull request',
+            '[#]num comments': 'Print comments on specified pull request',
+            }
 
     def __init__(self):
         super(PullsCommand, self).__init__()
@@ -33,12 +37,77 @@ class PullsCommand(Command):
         opts, args = self.parser.parse_args(args)
 
         if not args:
-            for p in self.repo.iter_pulls(opts.state, opts.number):
-                print(self.fs.format(p, u=p.user, bold=tc['bold'],
-                    default=tc['default']))
-                for f in p.iter_files():
-                    print('  {0}: +{1}/-{2}'.format(f.filename, f.additions,
-                        f.deletions))
+            return self.print_pulls(opts)
 
+        args[0] = trim_numbers(args[0])
+
+        if not args[0].isdigit():
+            return self.COMMAND_UNKNOWN
+
+        status = self.SUCCESS
+        number = args.pop(0)
+
+        subcmd = None
+        if args:
+            subcmd = args.pop(0).lower()
+
+        if not subcmd:
+            status = self.print_pull(number)
+        elif subcmd == 'comments':
+            status = self.print_comments(number)
+
+        return status
+
+    def format_short_pull(self, pull):
+        return self.fs.format(pull, u=pull.user, bold=tc['bold'],
+                default=tc['default'])
+
+    def get_pull(self, number):
+        pull = None
+        try:
+            pull = self.repo.pull_request(number)
+        except GitHubError as ghe:
+            if ghe.code == 404:
+                print("There is no pull request #{0} on {1}/{2}.".format(
+                    number, *self.repository))
+            raise ghe
+        return pull
+
+    def print_pull(self, number):
+        pull = self.get_pull(number)
+        if not pull:
+            return self.FAILURE
+
+        header = self.format_short_pull(pull)
+        body = '\n'.join(wrap(pull.body_text))
+        print('{0}\n{1}\n{2}\n'.format(header, sep, body))
+        print('{0}Files modified:{1}'.format(tc['underline'], tc['default']))
+        self.print_files(pull)
+        return self.SUCCESS
+
+    def print_pulls(self, opts):
+        for p in self.repo.iter_pulls(opts.state, opts.number):
+            print(self.format_short_pull(p))
+            self.print_files(p)
+        return self.SUCCESS
+
+    def print_files(self, pull):
+        for f in pull.iter_files():
+            print('  {0}: +{1}/-{2}'.format(f.filename, f.additions,
+                f.deletions))
+
+    def print_comments(self, number):
+        pull = self.get_pull(number)
+        if not pull:
+            return self.FAILURE
+
+        for c in pull.iter_comments():
+            fs = '@{uline}{u.login}{default} -- {date}\n{body}\n'
+            body = '\n'.join(wrap(c.body_text))
+            date = c.created_at.strftime('%Y-%m-%d %H:%M:%S')
+            print(fs.format(u=c.user, date=date, body=body,
+                uline=tc['underline'], default=tc['default']))
+
+        return self.SUCCESS
 
 PullsCommand()
