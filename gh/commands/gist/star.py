86cd1459fdedcac1f5fae91eac8db39dce145f0c
INADA Naoki <inada-n@klab.com>
2013-05-09 01:45:55 +0900

    Basic implementation of gist.star and gist.unstar

---
+++ gh/commands/gist/star.py
@@ -0,0 +1,36 @@
+from gh.base import Command
+
+class GistStarCommand(Command):
+    name = 'gist.star'
+    usage = '%prog [options] gist.star gist_id'
+    summary = 'Star the gist specified by gist_id'
+    subcommands = {}
+
+    def run(self, options, args):
+        opts, args = self.parser.parse_args(args)
+
+        if opts.help:
+            self.help()
+
+        if not args:
+            self.parser.error('You must provide a gist identifier')
+            return self.FAILURE
+
+        gist = self.gh.gist(args[0])
+        if not gist:
+            self.parser.error('Cound not retrieve gist:{0}'.format(gist))
+            return self.FAILURE
+
+        if gist.is_starred():
+            print("{0.html_url} is already starred.".format(gist))
+            return self.SUCCESS
+
+        if gist.star():
+            print("Starred:")
+            print('{0.html_url}: {0.description}'.format(gist))
+            return self.SUCCESS
+        else:
+            print("Failed to star.")
+            return self.FAILURE
+
+GistStarCommand()
---
+++ gh/commands/gist/unstar.py
@@ -0,0 +1,36 @@
+from gh.base import Command
+
+class GistUnstarCommand(Command):
+    name = 'gist.unstar'
+    usage = '%prog [options] gist.unstar gist_id'
+    summary = 'Unstar the gist specified by gist_id'
+    subcommands = {}
+
+    def run(self, options, args):
+        opts, args = self.parser.parse_args(args)
+
+        if opts.help:
+            self.help()
+
+        if not args:
+            self.parser.error('You must provide a gist identifier')
+            return self.FAILURE
+
+        gist = self.gh.gist(args[0])
+        if not gist:
+            self.parser.error('Cound not retrieve gist:{0}'.format(gist))
+            return self.FAILURE
+
+        if not gist.is_starred():
+            print("{0.html_url} is not starred.".format(gist))
+            return self.SUCCESS
+
+        if gist.unstar():
+            print("Unstarred:")
+            print('{0.html_url}: {0.description}'.format(gist))
+            return self.SUCCESS
+        else:
+            print("Failed to unstar.")
+            return self.FAILURE
+
+GistUnstarCommand()
